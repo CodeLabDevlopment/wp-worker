89c58cdf9768f7fcbd19f4b5d3986f151bb3772f
lucasmoraist <luksmnt1101@gmail.com>
2025-09-19 22:04:07 -0300

    first commit

---
+++ src/main/java/com/codelab/wp_worker/WpWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/wp_worker/WpWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
