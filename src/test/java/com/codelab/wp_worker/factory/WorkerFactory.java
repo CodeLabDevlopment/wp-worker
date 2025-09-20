3e0bbc6fa004e55b91dae9da7664cb1dacf8526d
lucasmoraist <luksmnt1101@gmail.com>
2025-09-20 09:42:22 -0300

    feat: add factory methods for creating WorkerEntity instances

--- src/test/java/com/codelab/wp_worker/factory/WorkerFactory.java
+++ src/test/java/com/codelab/wp_worker/factory/WorkerFactory.java
@@ -1,6 +1,7 @@
 package com.codelab.wp_worker.factory;
 
 import com.codelab.wp_worker.domain.model.Worker;
+import com.codelab.wp_worker.infrastructure.database.entity.WorkerEntity;
 
 import java.util.List;
 
@@ -15,6 +16,18 @@ public final class WorkerFactory {
                 new Worker(1L, "John Doe", 100.0),
                 new Worker(2L, "Jane Smith", 150.0),
                 new Worker(3L, "Alice Johnson", 200.0)
+        );
+    }
+
+    public static WorkerEntity createWorkerEntity() {
+        return new WorkerEntity(1L, "John Doe", 100.0);
+    }
+
+    public static List<WorkerEntity> createWorkerEntities() {
+        return List.of(
+                new WorkerEntity(1L, "John Doe", 100.0),
+                new WorkerEntity(2L, "Jane Smith", 150.0),
+                new WorkerEntity(3L, "Alice Johnson", 200.0)
         );
     }
 
